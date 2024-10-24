860440ae063e0a5cec90223d05b0886f39e6987b
vietnamdzjj <vietnamdzjj@gmail.com>
2024-10-24 21:19:36 +0700

    Update DVDs.java

--- DVDs.java
+++ DVDs.java
@@ -1,8 +1,22 @@
-
 public class DVDs { 
 	private String title; 
 	private String category; 
 	private String director; 
 	private int length; 
 	private float cost;
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
 }
