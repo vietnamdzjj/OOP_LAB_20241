8fe368f4da70e4da9f0be93e560d11a782a77091
vietnamdzjj <vietnamdzjj@gmail.com>
2024-10-24 21:45:10 +0700

    Update DVDs.java

--- DVDs.java
+++ DVDs.java
@@ -1,3 +1,4 @@
+
 public class DVDs { 
 	private String title; 
 	private String category; 
@@ -19,4 +20,30 @@ public class DVDs {
 	public float getCost() {
 		return cost;
 	}
+	public DVDs(String title) {
+		super();
+		this.title = title;
+	}
+	public DVDs(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+	public DVDs(String title, String category, String director, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.cost = cost;
+	}
+	public DVDs(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
+	
 }
